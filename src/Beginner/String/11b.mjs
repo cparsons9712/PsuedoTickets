5ce196695968e393037bb339604263af4adf8978
christine parsons <clong9712@gmail.com>
2024-08-23 10:48:01 -0400

    practice solving 11b

--- src/Beginner/String/11b.mjs
+++ src/Beginner/String/11b.mjs
@@ -1,5 +1,15 @@
 export const extractUsername = (email) => {
 
-    // your code here
 
+    if(typeof email != "string" ||
+        !email.includes("@") ||
+        email.startsWith("@")
+    ) return "Please enter a valid email address"
+
+
+    const substring = ""
+    for(let i = 0; i < email.indexOf("@"), i++;){
+        console.log(email[i])
+    }
+    return substring
 };
