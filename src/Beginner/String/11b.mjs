46609ea1eb54289f6b53be0d323a63c9cc17a90f
christine parsons <clong9712@gmail.com>
2024-08-23 10:48:17 -0400

    Clear 11b

--- src/Beginner/String/11b.mjs
+++ src/Beginner/String/11b.mjs
@@ -1,15 +1,3 @@
 export const extractUsername = (email) => {
 
-
-    if(typeof email != "string" ||
-        !email.includes("@") ||
-        email.startsWith("@")
-    ) return "Please enter a valid email address"
-
-
-    const substring = ""
-    for(let i = 0; i < email.indexOf("@"), i++;){
-        console.log(email[i])
-    }
-    return substring
 };
