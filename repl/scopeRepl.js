3fb8855d6497410b770f1f8f7efcbf5f6840fecd
christine parsons <clong9712@gmail.com>
2024-08-30 10:40:47 -0400

    scope repl

--- repl/scopeRepl.js
+++ repl/scopeRepl.js
@@ -1,2 +1,13 @@
-const reply = require("repl");
+const repl = require("repl");
 
+global.globalSentence = "This variable will be accessible anywhere because of the global. suffix"
+const explicitVariable = "This variable is being explicitly added to the repl's scope under the name exSentence"
+const readOnlyVariable = "Because we set the properties. Configurable = false means we cannot change the properties and enumerable means the properties can be looped over"
+
+const r = repl.start("scope-repl => ") //.context.exSentence = explicitVariable;
+
+Object.defineProperty(r.context, "readOnlyVariable", {
+    configurable : false,
+    enumerable : true,
+    value : readOnlyVariable,
+})
