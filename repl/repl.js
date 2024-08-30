e3061bf2373e4800d435f57e26c884d2087cfedc
christine parsons <clong9712@gmail.com>
2024-08-30 10:15:31 -0400

    give repl contextes

--- repl/repl.js
+++ repl/repl.js
@@ -1,2 +1,13 @@
 const repl = require("repl");
-repl.start("custom-repl => ");
+
+global.globalVariable = "This is a global string";
+
+const name = "Christine Parsons";
+
+const r = repl.start("custom-repl => ");
+
+Object.defineProperty(r.context, "name", {
+    configurable: false,
+    enumerable: true,
+    value: name,
+});
