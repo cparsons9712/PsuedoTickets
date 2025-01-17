0d9acf706c1b0c1bceb8fde6dbb3a18224af54b4
christine parsons <clong9712@gmail.com>
2025-01-17 10:33:55 -0500

    add description to getting started in the readme

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 
 ## Getting Started
+Navigate to a problem inside of the src file. The file directory is broken up by diffulty and problem type. At the top of each file is a commented out description and instructions. Complete the problem inside of the function, where you see the comment "Your code here". When you are ready to test your solution rul npm run test:grep -- @<problem ID>
 
-// COMING SOON
--- src/Beginner/String/11a.mjs
+++ src/Beginner/String/11a.mjs
@@ -1,31 +1,3 @@
-/*
-Scenerio:
-In a financial application, accurate handling of Social Security Numbers (SSNs)
-is critical. SSNs are used for identity verification, tax reporting, and various
-compliance-related processes. These numbers have a specific format: they are nine digits
-long, typically displayed as "XXX-XX-XXXX." Maintaining the integrity of this format is
-essential for proper data processing and validation.
-
-A common issue arises when SSNs are treated as numerical data. When this happens, leading
-zeros are stripped away, and formatting dashes are removed, resulting in incorrect and
-unusable data. For example, the SSN "012-34-5678" could be transformed into the number
-12345678, losing critical formatting information.
-
-To prevent such errors, it is necessary to verify that SSN inputs are treated as strings,
-preserving their exact format. This verification should be part of a generalized data type
-verification function that can be reused across different parts of the application,
-ensuring robust data handling.
-
-Problem:
-Write a function that returns true if the inputted data is a string, or returns false if it is not a string
-
-Testing:
-1. To test the code run the following command:
-npm run test:grep -- @11a
-
-*/
-
-
 export const isString = (input) => {
 
     // Your Code Here
